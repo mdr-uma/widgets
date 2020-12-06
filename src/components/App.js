ee9505034a373a0a99b1b0410579ce3ffe069744
github username <um_2003_8@hotmail.com>
2020-12-06 17:22:24 -0500

    add Accordion component & create items array

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import Accordion from './Accordion'
+
+const items = [
+    {
+        title: 'What is React?',
+        content: 'React is a front end javascript framework'
+    },
+
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library among engineers'
+    },
+    
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
 
 const App = () => {
-    return <h1>Widgets App</h1>
+    return (
+            <div>
+                <Accordion items={items}/>
+            </div>
+        )
 }
 
 export default App 
