eb1d3cc457a5086b7762facc4286ceaa3b055676
github username <um_2003_8@hotmail.com>
2020-12-10 21:55:08 -0500

    add input and classnames for styling

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Search = () => {
     return (
-        <div>Search</div>
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Search Term</label>
+                    <input className="input"/>
+                </div>
+            </div>
+        </div>
     )
 }
 
