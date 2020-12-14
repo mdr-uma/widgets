4950c74acf1b25d4c28a17aafe35f29ff837384a
github username <um_2003_8@hotmail.com>
2020-12-14 17:31:28 -0500

    pass props in destructuring method. create renderedOptions function that map options and list out all options with label.

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({ options }) => {
+    const renderedOptions = options.map((option) => {
+        return (
+            <div key={options.value} className="item" >
+                {option.lable}
+            </div>
+        )
+    })
+
     return(
         <div>
             <h1>Dropdown</h1>
