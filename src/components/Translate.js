353b7d86271f214b666fd1552fbacd6e95d6c6ae
github username <um_2003_8@hotmail.com>
2021-01-01 19:48:31 -0500

    create variable options to store array of abject that has label and value of languages.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+const options = [
+    {
+        label: 'Afrikaans',
+        value: 'af'
+    },
+    {
+        label: 'Arabic',
+        value: 'ar'
+    },
+    {
+        label: 'Hindi',
+        value: 'hi'
+    }
+]
+
 const Translate = () => {
     return(
         <div>
-            
+
         </div>
     )
 }
