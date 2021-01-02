555690b0344a88985aa1a03d7e2727dd3e0de1b5
github username <um_2003_8@hotmail.com>
2021-01-01 20:16:34 -0500

    create input with some style. Add state and set it to empty string.

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,9 +18,15 @@ const options = [
 
 const Translate = () => {
     const [language, setLanguage] = useState(options[0])
+    const [text, setText] = useState('')
 
     return(
-        <div>   
+        <div>
+            <div className="ui form">
+                <div className="field">
+                    <input value={text} onChange={(e) => setText(e.target.value)} />
+                </div>
+            </div>
             <Dropdown selected={language} onSelectedChange={setLanguage} options={options} />
         </div>
     )
