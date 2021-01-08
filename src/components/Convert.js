a5073f53bb299a49303c612287874f05a7e39542
github username <um_2003_8@hotmail.com>
2021-01-08 18:20:18 -0500

    create state using useState hook to keep track or update of text input. create useEffect function that fetch google api to get the data we need for transtlation of languages.

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Convert = ({ language, text }) => {
+    const [transleted, setTransleted] = useState("")
+    
     useEffect(() => {
-        console.log('New language or text');
+        const doTranslation = async () => {
+            const { data } = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: text,
+                    target: language.value,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms - IwDlM'
+                },
+            })
+
+            setTransleted(data.data.translations[0].translatedText)
+        }
+        doTranslation()
     }, [language, text])
+
     return(
         <div>
-
+            <h1 className="ui header">{transleted}</h1>
         </div>
     )
 }
